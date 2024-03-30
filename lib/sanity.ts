d9a657abd22b79224990394427647b41df6ff661
Mustapha tahiru <tahirumustapha13@gmail.com>
2024-03-30 14:11:29 +0000

    new change

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -3,12 +3,12 @@ import { createClient } from "next-sanity";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 // const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 // const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
 
 export const client = createClient({
   projectId:"y5xuhpyc",
-  dataset,
+  dataset:"production",
   apiVersion:"2024-03-24",
   useCdn: false,
 });
