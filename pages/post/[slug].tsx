2c5680f7402f649af9587195cb310f6521b4d334
Mustapha tahiru <tahirumustapha13@gmail.com>
2024-03-30 14:26:04 +0000

    new added changes

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -295,7 +295,7 @@ export async function getStaticPaths() {
   }));
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 }
 export const getStaticProps: GetStaticProps = async ({ params }) => {
