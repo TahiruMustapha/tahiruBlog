bcb8f94f482cd0fac073d267759b72e5da8bf26e
Mustapha tahiru <tahirumustapha13@gmail.com>
2024-03-30 12:27:59 +0000

    Made new changes

--- personalblog/sanity.config.ts
+++ personalblog/sanity.config.ts
@@ -3,12 +3,15 @@ import {structureTool} from 'sanity/structure'
 import {schemaTypes} from './schemaTypes'
 import { visionTool } from '@sanity/vision'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
 export default defineConfig({
   name: 'default',
   title: 'personalBlog',
 
-  projectId: 'y5xuhpyc',
-  dataset: 'production',
+  projectId,
+  dataset,
 
   plugins: [structureTool(), visionTool()],
 
